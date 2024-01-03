73299bbd948c194cc9152ada78cedbcc10ad5a28
lhaj-matich <choumouloussama@gmail.com>
2024-01-03 23:24:49 +0100

    commonCharacterCount.

---
+++ commonCharacterCount/index.ts
@@ -0,0 +1,13 @@
+function commonCharacterCount(s1: string, s2: string): number {
+    let count = 0;
+        
+    for (let i = 0; i < s1.length; i++)
+    {
+        if (s2.indexOf(s1[i]) != -1)
+        {
+            count += 1;
+            s2 = s2.replace(s1[i], '');
+        }
+    }
+    return (count);
+}
